6cadabc0bd6b2a89646d692540b8ac46547de541
nivetha198 <nivesj530@gmail.com>
2025-01-17 18:07:04 +0530

    adding files

---
+++ js/task.js
@@ -0,0 +1,58 @@
+let a = 10;
+let b = 40;
+console.log(a+b);
+
+let c = 20;
+let d = 15;
+console.log(c-d);
+
+let x = 10;
+let y = 5;
+console.log(x*y);
+
+console.log(a/b);
+
+let f = 2;
+let g = 5;
+console.log(2**5);
+
+let e = 17;
+let h = 20;
+console.log(e++);
+console.log(e++);
+
+let z = 10;
+console.log(z);
+
+let i = 8;
+console.log(i+=5);
+console.log(i);
+
+let j = 9;
+console.log(j-=3);
+console.log(j);
+
+let k = 10;
+console.log(k*=2);
+console.log(k);
+
+let l = 12;
+console.log(l/=4);
+console.log(l);
+
+let m = 10;
+console.log(m%=3);
+console.log(m);
+
+let n = 3;
+console.log(n**=3);
+console.log(n);
+
+
+
+
+
+
+
+
+
