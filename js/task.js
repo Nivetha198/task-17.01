7ba363ffcf2adbf36edfd3f60bef05b82f602524
Nivetha198 <nivesj530@gmail.com>
2025-01-19 13:28:56 +0530

    Add files via upload

--- js/task.js
+++ js/task.js
@@ -1,52 +1,33 @@
-let a = 10;
-let b = 40;
-console.log(a+b);
+let a = 5;
+let b = "5";
+console.log(a == b); //true
+console.log(a === b); //false
 
-let c = 20;
+// inequality check
+
+let c = 10;
 let d = 15;
-console.log(c-d);
+console.log(c!=d); //true
+console.log(25>20); //true
+console.log(8<12); //true
+console.log(30>=30); //true
+console.log(7<=10); //true
+console.log((10>5) && (20>15)); //true
+console.log((5>10) || (15>10)); //true
+console.log(!(10>20)); //true
+console.log((10>5 && 15>20) || 5<20); //true
+console.log(10>=25<=20); //true
+console.log(5 !== "5"); //true
+console.log(10>5 , 20>15 , 30<40); // true true true
+console.log((10>20) && (5<15)); //false
+console.log(!(10>20) && (5<15)); //true
+console.log(!(10>20)); //true
+console.log(!(5<15)); //false
 
-let x = 10;
-let y = 5;
-console.log(x*y);
 
-console.log(a/b);
 
-let f = 2;
-let g = 5;
-console.log(2**5);
 
-let e = 17;
-let h = 20;
-console.log(e++);
-console.log(e++);
 
-let z = 10;
-console.log(z);
-
-let i = 8;
-console.log(i+=5);
-console.log(i);
-
-let j = 9;
-console.log(j-=3);
-console.log(j);
-
-let k = 10;
-console.log(k*=2);
-console.log(k);
-
-let l = 12;
-console.log(l/=4);
-console.log(l);
-
-let m = 10;
-console.log(m%=3);
-console.log(m);
-
-let n = 3;
-console.log(n**=3);
-console.log(n);
 
 
 
